0581d3fa90ecae76ffe2b2b7ecf69db37d8f348e
maria clara lacerda <maria.pessoa007@cs.unipe.edu.br>
2025-11-18 10:07:09 -0300

    Create index2.php for user area interface

---
+++ index2.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Área Particular - TechFit</title>
+    <link rel="stylesheet" href="styleparticular.css">
+</head>
+<body>
+    <header class="topo">
+        <div class="logo">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEiwP0DDxMmfNvRfArEKbO49OavRQ8vOMM3A6pQZhYWggkZXYWUqAvFjVH_dWR6WH6OtJesS3IDLDadsxS7y4CzNuCsdVFIPfO7j6yJ72udeP-J3cV08sBSMx0hQLXp5JxOYYgOoQN1AIqcgFgFxjirp3KBptBi2eaOa12AjS49oubdpwItgi92MnNyqK-KY=w200-h185"  
+                 style="width: 100px; height: 100px; border-radius: 50%; object-fit: cover;" 
+                 alt="Logo TechFit">
+        </div>
+
+        <nav>
+          <div class="atalhos-btns">
+                <a href="treinadoress.php" class="btn">Treinadores</a>
+                <a href="horarios.php" class="btn">Horários</a>
+                <a href="contato.php" class="btn">Contato</a>
+                <a href="treinador.php" class="btn">Escolher treinador</a>
+            </div>
+    </header>
+
+    <main>
+        <section class="boas-vindas">
+            <h1>Bem-vindo(a) à sua área particular<span class="destaque"></span>!</h1>
+            <p>Continue sua jornada de evolução com suporte completo da TechFit 💪</p>
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2025 TechFit - Todos os direitos reservados.</p>
+    </footer>
+</body>
+</html>
