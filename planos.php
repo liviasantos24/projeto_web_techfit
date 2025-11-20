84ac4f58531c5ccbb064ab4d1ac0a8821f909801
maria clara lacerda <maria.pessoa007@cs.unipe.edu.br>
2025-11-19 21:24:53 -0300

    Create planos.php

---
+++ planos.php
@@ -0,0 +1,70 @@
+<section class="planos">
+    <h2>Nossos Planos</h2>
+
+    <div class="container-planos">
+
+        <!-- Plano Black -->
+        <div class="card-plano destaque">
+            <h3>Plano Black</h3>
+
+            <p class="preco">
+                <span class="valor">R$ 9,90*</span><br>
+                no 1º mês, depois R$ 149,90/mês
+            </p>
+
+            <ul class="lista">
+                <li>✔ Fidelidade</li>
+                <li>✔ Acesso ilimitado a todas as unidades</li>
+                <li>✔ Leve 5 amigos por mês</li>
+                <li>✔ Cadeira de massagem</li>
+                <li>✔ Área de musculação e aeróbicos</li>
+                <li>✔ App TechFit</li>
+            </ul>
+
+            <button class="btn-contratar">Contratar</button>
+        </div>
+
+        <!-- Plano Fit -->
+        <div class="card-plano">
+            <h3>Plano Fit</h3>
+
+            <p class="preco">
+                <span class="valor">R$ 9,90*</span><br>
+                no 1º mês, depois R$ 99,90/mês
+            </p>
+
+            <ul class="lista">
+                <li>✔ Fidelidade</li>
+                <li>✖ Acesso ilimitado</li>
+                <li>✖ Levar amigos</li>
+                <li>✖ Cadeira de massagem</li>
+                <li>✔ Área de musculação e aeróbicos</li>
+                <li>✔ App TechFit</li>
+            </ul>
+
+            <button class="btn-contratar">Contratar</button>
+        </div>
+
+        <!-- Plano Smart -->
+        <div class="card-plano">
+            <h3>Plano Smart</h3>
+
+            <p class="preco">
+                <span class="valor">R$ 9,90*</span><br>
+                no 1º mês, depois R$ 119,90/mês
+            </p>
+
+            <ul class="lista">
+                <li>✖ Fidelidade</li>
+                <li>✖ Acesso ilimitado</li>
+                <li>✖ Levar amigos</li>
+                <li>✖ Cadeira de massagem</li>
+                <li>✔ Área de musculação e aeróbicos</li>
+                <li>✔ App TechFit</li>
+            </ul>
+
+            <button class="btn-contratar">Contratar</button>
+        </div>
+
+    </div>
+</section>
