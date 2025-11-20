f7f84e79053c69b50484950d2d70ad816bd5f751
maria clara lacerda <maria.pessoa007@cs.unipe.edu.br>
2025-11-19 21:23:45 -0300

    Add initial index.php for TechFit website

---
+++ index.php
@@ -0,0 +1,135 @@
+<?php
+require_once 'config/db.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Centro Esportivo TechFit</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/inicio/style.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEg3xbKPWUZ9OJfhNpM3vlqZkjV7JvguHGiSWKumgauhf94ioe89D3cYsCXXdefkCcXVbeyHBX-3iIPY8Ukwc3j7tQGtFitVcB6iwMvzSp6TmK1GMDMlaVBGG3ZYSeLrdV2iKyi_QfuzO3q353dvYXi0pIosrN8SexBRKfI_L66ZXA0aYJFuASOZJXGUobkm"  
+                 style="width: 150px; height: 150px; border-radius: 50%; object-fit: cover;" 
+                 alt="Logo TechFit">
+        </div>
+
+        <nav>
+            <a href="index.php" class="active">Home</a>
+            <a href="planos.php">Planos</a>
+            <a href="aulas.php">Aulas e treinos exclusivos</a>
+            <a href="treinadores.php">Personais</a>
+            <a href="horariosss.php">Aula Coletiva</a>
+            <a href="contato.php">Contato</a>
+            <a href="cadastro.php" class="login-btn">Cadastro</a>
+            <a href="login.php">Login</a>
+        </nav>
+    </header>
+
+<section class="apresentacao">
+    <div class="conteudo-apresentacao">
+        <div class="texto">
+            <h1>Bem-vindo à <span class="destaque">TechFit</span></h1>
+            <h2>Nossa Academia</h2>
+            <p>
+                A <strong> TechFit é uma academia moderna, equipada e preparada para oferecer treinos de qualidade em um ambiente confortável e motivador. Contamos com estrutura completa e uma equipe qualificada para acompanhar sua evolução.</p>
+
+<p>Nossa grade de aulas inclui Spinning, Funcional, Jump, GAP, Dança Ritmos, Step, Core & Mobilidade e Grupo de Corrida, garantindo opções para todos os objetivos, desde condicionamento até alta performance.
+
+Na TechFit, você encontra a combinação ideal entre estrutura, variedade e resultados.</p></strong> 
+            </p>
+        </div>
+        <div class="imagem">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEg3xbKPWUZ9OJfhNpM3vlqZkjV7JvguHGiSWKumgauhf94ioe89D3cYsCXXdefkCcXVbeyHBX-3iIPY8Ukwc3j7tQGtFitVcB6iwMvzSp6TmK1GMDMlaVBGG3ZYSeLrdV2iKyi_QfuzO3q353dvYXi0pIosrN8SexBRKfI_L66ZXA0aYJFuASOZJXGUobkm" alt="Fachada da TechFit">
+        </div>
+    </div>
+</section>
+
+    <!-- Seção 2 - Estrutura -->
+    <section class="estrutura">
+        <h2>Nossa Estrutura</h2>
+        <p>
+
+        </p>
+        <div class="galeria">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEhTRB896aODM91TKidv8wlbu-egHHZXGbSzY8-3ykdvMY4kBlbRR12UUDnIqVK-RQdOgiNxrVIvbTog_xrb_FdhCQs3c5LMPw2p8qxd7hK9jxU6gS6kz0F6BkZC27bw3-Vr5H__rvV9uHtmrtSWL4qbvoUvu57eDP1T5UjEFmFzz_Un82spkNjsqO-PFaBr" alt="Academia">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEipwfDR8gbZT1v94M2SSrzvFUT9EwUdcQdB0S1dbGBchJGatoelfO0gMIXBQCAUAMDIoPqb1mYzDzXqXN8bLtlLlCzwVGQk2u7_zjU0jO3bCZt0YVEZ76WaP3pi2SF60HOou4TxbbLqvlD5Jmdl9GfrwZzJVzeZXI67XJbv-swrcnDuUnpN1wsWhtQA58Dy" alt="Academia2">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEjdI98OcdEjdctxXBM35BfSYQDtoRJgI-qR04EFXXJLbLOzkTjuOG9rUtEb0VBrSDo3XiQDZxJyc0iWvPj7RfTYmowR51kJeuEKA40kg5BrODqhPPtgitPGnEhEEA79ISJRxrbRLwpBpc3Wq7uWzHKMOvsLQD7r-O7H1qGgIjTFt2gJLuj9r-8qWnFYWxZL" alt="Recepção">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEiv8TmnzFwUk8xDCt4S50jrCGsNTTNzGwFh06ayTArB_FrwOuRKdlBl4RrzWj2hwBN03KhbUhYzlYHeD6JW0PhRBqkevJpSaY_hsZiWU884fD352o0AdRz3YYppAWD98iRImsXvR2ItBpeDkg9tLxrfrJv7X2FohGTlx38Z1tD7GuugpxNYuOqUbvEbixbx" alt="Pula Pula">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEhEcNSUxXK17CbyzHJo2bVPzj3CvGN4qrUfZg70VaP3I2T6ld6Sr36b8YYdLW61Rsdyx9MMjYGk_3MMS9f59KOyw0eureYuSoee_En5BNB-94KD3fYXk4ayxJXeDU-wG6brHo4Befxe3q9488J8tiofMMDtLKIfNPiAXQKhj53V5LtMAtI8IP6UktuhHTxH" alt="mobilidade?">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEilSZOX1eqXrBItNb7I5LJ1GCwmsMrmWO4COHAKdvvtKX4C4dE7Vr8pKIo1kSskOhMuMDX_sVf_bQzBqxUvR_tZQN0Baikk6bpzyMhIMVEeAcX5FepePbmBY9BrPPtlaWdIpB5C4VadobO1xcjkS8Xm85StEL-AhjYD5SiahE_ifTbJ5zrPit7bGvBMJPZ3" alt="Bicicleta">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEgnE1to0cT-540k-iA3MsvRbHZwhy5V5YPJGZuDDij3rVf9nk5Q6TPD6OU_jNjpZevmHSEH0JNV0Ha0zaXTzkyhJ1MmeDcrvXQQsaKHa3SxPngqgQrrxfsaO8mH5Om7UJJm-e95Tsv6IGVGrcTiIebqAcV7OnHwnjJJbIp9fJYdI4zMUyQljL0Vw1ui1W9j" alt="Corrida">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEhKSW-AHD5n6O6KN-88PTq8IF-VTLEXC4HbEGvVkmC-eQ7WUW94WkoDclre6IhoP08NUmut_FUVmg-sVVJPubwCHbIte5uMed8ZYNqrD8axw4PNdXYiPWAi41W_M_tJKKxWNROr2X4ftiIjfDHdcEQLx0Fop9FM_akUz3Xmi8Fy2_y-US8OAK2XPo1vXhDy" alt="Dança">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEjgWX2OKsAc0seIV0Zv9gTpP2kk8VPINTtjPo6yPvEfp2fmTx5puGz9k0_ZVJEnk7-FkvxavzinDahPTJXQ7LqaSNARYgQXboK55z9oaNYVW6SCnR3hm3MZUPe1ezPjzn9L5UkK5FM4kxxchthOVP-F3zm40ByDvY3keV1WDrjbKYX3mFZQXYOAoMDpokMW" alt="Tapete">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEjS0ZTbzU0kuf4kqnLwKXdGxaFOTXlHX8eMKEjO-4ND8cGom6xcRj0L4ESoSrhdiQGFfRdjg8-PL-HaSOk6tTqMU3q73UrMO3AfCud5OEYC9_P3EwjN09OJyK6HuarPvF8wiIhL0JueUsNL5hAAKDidweenKebl_saqR7xvh3-V1FwDE3xcrA2NCes4TOuZ" alt="Corda">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEjj53LELWpkBBtD83KBU4mkZ037ldxSyZUoac_CGl2FNvj0pnsEvLyvhR3abhBHxiyIuer3Jt_7pdSX7rbLh88YrLSryQQ4ArM0-J5Ey9Ea3UM5bkyX8xo66t7XajijR8c9Oor7BLqL-O8hizt6rC0qZAIKyNyaJNukkLenSkr5xi6O2sxS_v4qsD3KfS2g" alt="Dança2">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEhirCt2lXNsW21DmMRnnvx4ENCsTAZe6Ga5LvI7_C2V9WeZKCLAiWfrQ3sNMelIFL5z3gNvV4j2SwdntYvSB3qtalzp4QjlevkvDT5zK5tN74BnILlycpLxqtR5kuzzl9Cm6L3XfULzcvyMI0FZSJjgg4n0WMet2Oo8R63mUS8Zxjj9xpzzVCEY2cI6Pecb" alt="Banheiro Feminino">
+            <img src="https://blogger.googleusercontent.com/img/a/AVvXsEgt1sP9w6vojVGIu5scOhoc3H_Sq-WQjJnlh52nzavazFomZnZDzGZBoYUEPULnclDZVkuYDPnB5dyXOjlJQOwWo60QzMVdfnAcXfQeoN4vtIenEaSQdiqZOr2O472Z4NgliYmwK47fjleYWW9iVkPK4_cZzoWO3B261-cTpdaG5LqCgKUqy-TYqiwNumx5" alt="Banheiro Masculino">
+          
+        </div>
+    </section>
+
+    <!-- Seção 3 - Modalidades -->
+    <section class="modalidades">
+        <h2>Experiência</h2>
+        <div class="cards">
+
+            <div class="card">
+                 <img src="https://blogger.googleusercontent.com/img/a/AVvXsEilSZOX1eqXrBItNb7I5LJ1GCwmsMrmWO4COHAKdvvtKX4C4dE7Vr8pKIo1kSskOhMuMDX_sVf_bQzBqxUvR_tZQN0Baikk6bpzyMhIMVEeAcX5FepePbmBY9BrPPtlaWdIpB5C4VadobO1xcjkS8Xm85StEL-AhjYD5SiahE_ifTbJ5zrPit7bGvBMJPZ3" >
+                <h3>Por que fazer aula de Spinning?</h3>
+                <p>A aula de Spinning da TechFit acontece em um estúdio imersivo com luzes neon e visual futurista. É excelente para queimar calorias, melhorar o condicionamento e fortalecer pernas e glúteos. Em média, você pode queimar entre 400 e 700 calorias por aula.</p>
+            </div>
+    
+             <div class="card">
+                <img src="https://blogger.googleusercontent.com/img/a/AVvXsEjS0ZTbzU0kuf4kqnLwKXdGxaFOTXlHX8eMKEjO-4ND8cGom6xcRj0L4ESoSrhdiQGFfRdjg8-PL-HaSOk6tTqMU3q73UrMO3AfCud5OEYC9_P3EwjN09OJyK6HuarPvF8wiIhL0JueUsNL5hAAKDidweenKebl_saqR7xvh3-V1FwDE3xcrA2NCes4TOuZ">
+                <h3>Por que fazer aula de Funcional?</h3>
+                <p>A aula de Funcional trabalha força, velocidade, explosão e resistência. Os treinos variam a cada dia e ativam todo o corpo, ajudando no aumento de massa magra, definição muscular e alto gasto calórico.</p>
+            </div>
+
+
+            <div class="card">
+                <img src="https://blogger.googleusercontent.com/img/a/AVvXsEiv8TmnzFwUk8xDCt4S50jrCGsNTTNzGwFh06ayTArB_FrwOuRKdlBl4RrzWj2hwBN03KhbUhYzlYHeD6JW0PhRBqkevJpSaY_hsZiWU884fD352o0AdRz3YYppAWD98iRImsXvR2ItBpeDkg9tLxrfrJv7X2FohGTlx38Z1tD7GuugpxNYuOqUbvEbixbx">
+                <h3>Por que fazer aula de Jump?</h3>
+                <p>Uma aula dinâmica e divertida que melhora o condicionamento, ativa pernas e glúteos, e elimina até 600 calorias por treino. O impacto é reduzido e ideal para quem quer suar muito com segurança</p>
+            </div>
+
+            <div class="card">
+                <img src="https://blogger.googleusercontent.com/img/a/AVvXsEhKSW-AHD5n6O6KN-88PTq8IF-VTLEXC4HbEGvVkmC-eQ7WUW94WkoDclre6IhoP08NUmut_FUVmg-sVVJPubwCHbIte5uMed8ZYNqrD8axw4PNdXYiPWAi41W_M_tJKKxWNROr2X4ftiIjfDHdcEQLx0Fop9FM_akUz3Xmi8Fy2_y-US8OAK2XPo1vXhDy">
+                <h3>Por que fazer aula de Dança Ritmos?</h3>
+                <p>Uma aula vibrante com coreografias energéticas e iluminação futurista. Além de divertida, ajuda na perda de peso, coordenação e melhora da autoestima.</p>
+            </div>
+
+            <div class="card">
+                <img src="https://blogger.googleusercontent.com/img/a/AVvXsEhEcNSUxXK17CbyzHJo2bVPzj3CvGN4qrUfZg70VaP3I2T6ld6Sr36b8YYdLW61Rsdyx9MMjYGk_3MMS9f59KOyw0eureYuSoee_En5BNB-94KD3fYXk4ayxJXeDU-wG6brHo4Befxe3q9488J8tiofMMDtLKIfNPiAXQKhj53V5LtMAtI8IP6UktuhHTxH">
+                <h3>Por que fazer aula de GAP?</h3>
+                <p>O treino de Glúteo, Abdômen e Pernas foca totalmente em definição e fortalecimento. Ideal para quem busca hipertrofia localizada e aumento de resistência.</p>
+            </div>
+
+            
+            <div class="card">
+                <img src="https://blogger.googleusercontent.com/img/a/AVvXsEjj53LELWpkBBtD83KBU4mkZ037ldxSyZUoac_CGl2FNvj0pnsEvLyvhR3abhBHxiyIuer3Jt_7pdSX7rbLh88YrLSryQQ4ArM0-J5Ey9Ea3UM5bkyX8xo66t7XajijR8c9Oor7BLqL-O8hizt6rC0qZAIKyNyaJNukkLenSkr5xi6O2sxS_v4qsD3KfS2g">
+                <h3>Por que fazer aula de Step?</h3>
+                <p>A aula de Step combina movimentos coreografados com elevação de plataforma, promovendo resistência, coordenação e alta queima calórica.</p>
+            </div>
+
+            <div class="card">
+                 <img src="https://blogger.googleusercontent.com/img/a/AVvXsEjgWX2OKsAc0seIV0Zv9gTpP2kk8VPINTtjPo6yPvEfp2fmTx5puGz9k0_ZVJEnk7-FkvxavzinDahPTJXQ7LqaSNARYgQXboK55z9oaNYVW6SCnR3hm3MZUPe1ezPjzn9L5UkK5FM4kxxchthOVP-F3zm40ByDvY3keV1WDrjbKYX3mFZQXYOAoMDpokMW">
+                 <h3>Por que fazer aula de Core & Mobilidade?</h3>
+                <p>Treino focado em fortalecimento profundo do abdômen e na melhora da flexibilidade e mobilidade articular. Essencial para prevenir dores e melhorar a postura.</p>
+            </div>
+
+            <div class="card">
+                <img src="https://blogger.googleusercontent.com/img/a/AVvXsEgnE1to0cT-540k-iA3MsvRbHZwhy5V5YPJGZuDDij3rVf9nk5Q6TPD6OU_jNjpZevmHSEH0JNV0Ha0zaXTzkyhJ1MmeDcrvXQQsaKHa3SxPngqgQrrxfsaO8mH5Om7UJJm-e95Tsv6IGVGrcTiIebqAcV7OnHwnjJJbIp9fJYdI4zMUyQljL0Vw1ui1W9j">
+                <h3>Por que participar do Grupo de Corrida?</h3>
+                <p>Treinos ao ar livre com acompanhamento e ambiente futurista TechFit. Ideal para condicionamento, resistência e queima calórica intensa.</p>
+            </div>
+        </div>      
+  </div>
+</section>
+</section>
+</body>
+</html>
+</html>
